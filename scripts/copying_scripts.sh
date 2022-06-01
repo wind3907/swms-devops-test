1dad16f8796396acceece0d871a611ff03a4fadb
Wimukthi Indeewara <wind3907>
2022-06-01 15:52:31 +0530

    test

--- scripts/copying_scripts.sh
+++ scripts/copying_scripts.sh
@@ -1,2 +1,2 @@
-ssh -i $SSH_KEY ${SSH_KEY_USR}@rs1060b1.na.sysco.net ". ~/.profile; beoracle_ci mkdir -p /tempfs/11gtords"
-scp -i $SSH_KEY ${WORKSPACE}/scripts/* ${SSH_KEY_USR}@rs1060b1.na.sysco.net:/tempfs/11gtords/
+ssh -i $SSH_KEY ${SSH_KEY_USR}@rs1060b1.na.sysco.net ". ~/.profile; beoracle_ci mkdir -p /home2/dba/jcx/11gtords/test"
+scp -i $SSH_KEY ${WORKSPACE}/scripts/* ${SSH_KEY_USR}@rs1060b1.na.sysco.net:/home2/dba/jcx/11gtords/test/
