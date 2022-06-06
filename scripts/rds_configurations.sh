853c924bf16310192797a3b862a2fe714352072e
Wimukthi Indeewara <wind3907>
2022-06-06 15:13:33 +0530

    test

--- scripts/rds_configurations.sh
+++ scripts/rds_configurations.sh
@@ -9,8 +9,8 @@ export TNS_ADMIN=/home2/dba/jcx/11gtords
 export ORACLE_SID=swms_ci1
 
 sqlplus root/$ROOTPW@$TARGETDB << EOF
-@rdsconfig/type_log_message.sql
-@rdsconfig/create_async_log_sq_ddl.sql
-@rdsconfig/grants_for_aq.sql
+@/home2/dba/jcx/11gtords/rdsconfig/type_log_message.sql
+@/home2/dba/jcx/11gtords/rdsconfig/create_async_log_sq_ddl.sql
+@/home2/dba/jcx/11gtords/rdsconfig/grants_for_aq.sql
 exit
 EOF
