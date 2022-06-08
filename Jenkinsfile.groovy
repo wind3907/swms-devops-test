d3371c9e819b6c926ec9ea4a6b1aeb54c1535d1e
Wimukthi Indeewara <wind3907>
2022-06-08 15:22:33 +0530

    Hi

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,39 +1,46 @@
+properties(
+    [
+        buildDiscarder(logRotator(numToKeepStr: '20')),
+        parameters(
+            [
+                string(name: 'SOURCE_DB', defaultValue: 'rsxxxe', description: 'Source Database. eg: rs040e'),
+                string(name: 'TARGET_DB', defaultValue: 'rds_trn_xxx', description: 'Target Database. eg: rds_trn_040'),
+                string(name: 'ROOT_PW', defaultValue: 'SwmsRoot123', description: 'Root Password'),
+                string(name: 'TARGET_SERVER', defaultValue: 'lxxxxtrn', description: 'Host ec2 instance. eg: lx036trn'),
+                choice(name: 'artifact_s3_bucket', choices: ['swms-build-artifacts', 'swms-build-dev-artifacts'], description: 'The build\'s targeted platform'),
+                choice(name: 'platform', choices: ['linux','aix_11g_11g', 'aix_19c_12c'], description: 'The build\'s targeted platform'),
+                string(name: 'artifact_version', defaultValue: '50_0', description: 'The swms version to deploy', trim: true),
+                [
+                            name: 'artifact_name',
+                            description: 'The name of the artifact to deploy',
+                            $class: 'CascadeChoiceParameter',
+                            choiceType: 'PT_SINGLE_SELECT',
+                            filterLength: 1,
+                            filterable: false,
+                            randomName: 'choice-parameter-artifact_name',
+                            referencedParameters: 'artifact_s3_bucket, platform, artifact_version',
+                            script: [
+                                $class: 'GroovyScript',
+                                script: [classpath: [], sandbox: false, script: '''\
+                                        if (platform?.trim() && artifact_version?.trim()) {
+                                            def process = "aws s3api list-objects --bucket ${artifact_s3_bucket} --prefix ${platform}-${artifact_version} --query Contents[].Key".execute()
+                                            return process.text.replaceAll('"', "").replaceAll("\\n","").replaceAll(" ","").tokenize(',[]')
+                                        } else {
+                                            return []
+                                        }
+                                    '''.stripIndent()
+                                ]
+                            ]
+                        ],
+                string(name: 'dba_masterfile_names', description: 'The name of the artifact to deploy', defaultValue: 'R50_0_dba_master.sql', trim: true),
+                string(name: 'master_file_retry_count', description: 'Amount of attempts to apply the master file. This is setup to handle circular dependencies by running the same master file multiple times.', defaultValue: '3', trim: true)
+
+            ]
+        )
+    ]
+)
 pipeline {
     agent { label 'master' }
-    parameters {
-        string(name: 'SOURCE_DB', defaultValue: 'rsxxxe', description: 'Source Database. eg: rs040e')
-        string(name: 'TARGET_DB', defaultValue: 'rds_trn_xxx', description: 'Target Database. eg: rds_trn_040')
-        string(name: 'ROOT_PW', defaultValue: 'SwmsRoot123', description: 'Root Password')
-        string(name: 'TARGET_SERVER', defaultValue: 'lxxxxtrn', description: 'Host ec2 instance. eg: lx036trn')
-        choice(name: 'artifact_s3_bucket', choices: ['swms-build-artifacts', 'swms-build-dev-artifacts'], description: 'The build\'s targeted platform')
-        choice(name: 'platform', choices: ['linux','aix_11g_11g', 'aix_19c_12c'], description: 'The build\'s targeted platform')
-        string(name: 'artifact_version', defaultValue: '50_0', description: 'The swms version to deploy', trim: true)
-        [
-                    name: 'artifact_name',
-                    description: 'The name of the artifact to deploy',
-                    $class: 'CascadeChoiceParameter',
-                    choiceType: 'PT_SINGLE_SELECT',
-                    filterLength: 1,
-                    filterable: false,
-                    randomName: 'choice-parameter-artifact_name',
-                    referencedParameters: 'artifact_s3_bucket, platform, artifact_version',
-                    script: [
-                        $class: 'GroovyScript',
-                        script: [classpath: [], sandbox: false, script: '''\
-                                if (platform?.trim() && artifact_version?.trim()) {
-                                    def process = "aws s3api list-objects --bucket ${artifact_s3_bucket} --prefix ${platform}-${artifact_version} --query Contents[].Key".execute()
-                                    return process.text.replaceAll('"', "").replaceAll("\\n","").replaceAll(" ","").tokenize(',[]')
-                                } else {
-                                    return []
-                                }
-                            '''.stripIndent()
-                        ]
-                    ]
-                ]
-        string(name: 'dba_masterfile_names', description: 'The name of the artifact to deploy', defaultValue: 'R50_0_dba_master.sql', trim: true)
-        string(name: 'master_file_retry_count', description: 'Amount of attempts to apply the master file. This is setup to handle circular dependencies by running the same master file multiple times.', defaultValue: '3', trim: true)
-
-    }
     environment {
         SSH_KEY = credentials('/swms/jenkins/swms-universal-build/svc_swmsci_000/key')  
     }
