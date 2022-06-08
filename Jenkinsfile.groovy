140c321e8ea96fbdaaf322cfcaa238b2543aad4f
Wimukthi Indeewara <wind3907>
2022-06-08 13:25:18 +0530

    Hi

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -38,40 +38,40 @@ pipeline {
                 sh '${WORKSPACE}/scripts/copying_scripts.sh'
             }
         }
-        stage('Execute 45 Script') {
-            steps {
-                echo "Section: Execute 45 Script"
-                sh """
-                    scp -i $SSH_KEY ${WORKSPACE}/scripts/all_target_45_2.sh ${SSH_KEY_USR}@${params.HOST}.swms-np.us-east-1.aws.sysco.net:/tempfs/
-                """
-                timeout(time: 3, unit: 'MINUTES') {
-                    sh """
-                        ssh -i $SSH_KEY ${SSH_KEY_USR}@${params.HOST}.swms-np.us-east-1.aws.sysco.net "
-                        /ts/curr/bin/beswms_ci cp /tempfs/all_target_45_2.sh /swms/curr/schemas/;
-                        /ts/curr/bin/beswms_ci /swms/curr/schemas/all_target_45_2.sh swms swms;
-                        "
-                    """
-                }       
-            }
-        }
-        stage('RDS Configurations') {
-            steps {
-                echo "Section: RDS Configurations"
-                dir("swms-opco")
-                {
-                    git branch: 'develop',
-                    credentialsId: scm.getUserRemoteConfigs()[0].getCredentialsId(),
-                    url: 'git@github.com:SyscoCorporation/swms-opco.git'
-                }
-                sh 'scp -i $SSH_KEY ${WORKSPACE}/swms-opco/configuration/rds/queues/* ${SSH_KEY_USR}@rs1060b1.na.sysco.net:/home2/dba/jcx/11gtords/rdsconfig/'
-                sh """
-                    ssh -i $SSH_KEY ${SSH_KEY_USR}@rs1060b1.na.sysco.net "
-                    . ~/.profile;
-                    beoracle_ci /tempfs/11gtords/rds_configurations.sh ${params.SOURCE_DB} ${params.TARGET_DB} ${params.ROOT_PW} '/tempfs/DBBackup/SWMS/swm1_db_${params.SOURCE_DB}*.tar.gz'
-                    "
-                """           
-            }
-        }
+        // stage('Execute 45 Script') {
+        //     steps {
+        //         echo "Section: Execute 45 Script"
+        //         sh """
+        //             scp -i $SSH_KEY ${WORKSPACE}/scripts/all_target_45_2.sh ${SSH_KEY_USR}@${params.HOST}.swms-np.us-east-1.aws.sysco.net:/tempfs/
+        //         """
+        //         timeout(time: 3, unit: 'MINUTES') {
+        //             sh """
+        //                 ssh -i $SSH_KEY ${SSH_KEY_USR}@${params.HOST}.swms-np.us-east-1.aws.sysco.net "
+        //                 /ts/curr/bin/beswms_ci cp /tempfs/all_target_45_2.sh /swms/curr/schemas/;
+        //                 /ts/curr/bin/beswms_ci /swms/curr/schemas/all_target_45_2.sh swms swms;
+        //                 "
+        //             """
+        //         }       
+        //     }
+        // }
+        // stage('RDS Configurations') {
+        //     steps {
+        //         echo "Section: RDS Configurations"
+        //         dir("swms-opco")
+        //         {
+        //             git branch: 'develop',
+        //             credentialsId: scm.getUserRemoteConfigs()[0].getCredentialsId(),
+        //             url: 'git@github.com:SyscoCorporation/swms-opco.git'
+        //         }
+        //         sh 'scp -i $SSH_KEY ${WORKSPACE}/swms-opco/configuration/rds/queues/* ${SSH_KEY_USR}@rs1060b1.na.sysco.net:/home2/dba/jcx/11gtords/rdsconfig/'
+        //         sh """
+        //             ssh -i $SSH_KEY ${SSH_KEY_USR}@rs1060b1.na.sysco.net "
+        //             . ~/.profile;
+        //             beoracle_ci /tempfs/11gtords/rds_configurations.sh ${params.SOURCE_DB} ${params.TARGET_DB} ${params.ROOT_PW} '/tempfs/DBBackup/SWMS/swm1_db_${params.SOURCE_DB}*.tar.gz'
+        //             "
+        //         """           
+        //     }
+        // }
         stage('Reset Hash Password') {
             steps {
                 echo "Section: Reset Hash Password"
