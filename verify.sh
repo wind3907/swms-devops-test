d3c3b9bcfc2ef63c3a81f7cbd97379e8d4f24395
Wimukthi Indeewara <wind3907>
2022-06-22 17:31:25 +0530

    test

--- verify.sh
+++ verify.sh
@@ -1,7 +1,6 @@
 TARGETDB='lx739q13'
-ROOTPW='ZXCK>MW*$uo%B6t?'
+ROOTPW='ZXCK>MW*$uo%B6t?s'
 
 sqlplus 'root/'"$ROOTPW"'@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST='"$TARGETDB"'-db.swms-np.us-east-1.aws.sysco.net)(PORT='1521'))(CONNECT_DATA=(SID='SWM1')))'  << EOF
-SELECT OPS$SWMS FROM dba_users;
 exit
 EOF
