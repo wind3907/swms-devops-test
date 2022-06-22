6f3c7c459951bfd8b4323a756dafa1c921e5fe6d
Wimukthi Indeewara <wind3907>
2022-06-22 16:56:30 +0530

    test

--- verify.sh
+++ verify.sh
@@ -1,8 +1,8 @@
-TARGETDB=$1
+TARGETDB="lx739q13-db.swms-np.us-east-1.aws.sysco.net:1521/SWM1"
 ROOTPW="ZXCK>MW*$uo%B6t?"
 
 echo "Hi Im inside"
 
-sqlplus "root/$ROOTPW@lx739q13-db.swms-np.us-east-1.aws.sysco.net:1521/SWM1" << EOF
+sqlplus root/$ROOTPW@$TARGETDB << EOF
 exit
 EOF
