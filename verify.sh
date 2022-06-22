8c3b21d61cfcb839e93fe18e4eedd9f58a7e45a9
Wimukthi Indeewara <wind3907>
2022-06-22 16:58:21 +0530

    test

--- verify.sh
+++ verify.sh
@@ -3,6 +3,8 @@ ROOTPW="ZXCK>MW*$uo%B6t?"
 
 echo "Hi Im inside"
 
-sqlplus root/$ROOTPW@$TARGETDB << EOF
+# sqlplus root/$ROOTPW@$TARGETDB
+
+sqlplus 'root/ZXCK>MW*$uo%B6t?@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST='lx739q13-db.swms-np.us-east-1.aws.sysco.net')(PORT='1521'))(CONNECT_DATA=(SID='SWM1')))'  << EOF
 exit
 EOF
