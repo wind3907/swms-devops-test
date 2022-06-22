d12eee4d088f9ae01601550accc97d1f8a604d6b
Wimukthi Indeewara <wind3907>
2022-06-22 20:52:26 +0530

    test

--- verify.sh
+++ verify.sh
@@ -1,5 +1,5 @@
 TARGETDB=$1
-ROOTPW=$2
+ROOTPW='ZXCK>MW*$uo%B6t?'
 
 echo "Hiii"
 echo "${TARGETDB}"
@@ -8,6 +8,6 @@ echo "${ROOTPW}"
 export ORACLE_SID=swms_ci1
 export TNS_ADMIN=/home2/dba/jcx/11gtords
 
-sqlplus 'root/ZXCK>MW*$uo%B6t?@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST='$TARGETDB-db.swms-np.us-east-1.aws.sysco.net')(PORT='1521'))(CONNECT_DATA=(SID='SWM1')))' << EOF
+sqlplus 'root/'$ROOTPW'@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST='$TARGETDB-db.swms-np.us-east-1.aws.sysco.net')(PORT='1521'))(CONNECT_DATA=(SID='SWM1')))' << EOF
 exit
 EOF
