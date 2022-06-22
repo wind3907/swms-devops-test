1987ae1b003514c01c08d43a581baed235b4408d
Wimukthi Indeewara <wind3907>
2022-06-22 16:55:02 +0530

    test

--- verify.sh
+++ verify.sh
@@ -3,6 +3,6 @@ ROOTPW="ZXCK>MW*$uo%B6t?"
 
 echo "Hi Im inside"
 
-sqlplus 'root/$ROOTPW@lx739q13-db.swms-np.us-east-1.aws.sysco.net:1521/SWM1' << EOF
+sqlplus "root/$ROOTPW@lx739q13-db.swms-np.us-east-1.aws.sysco.net:1521/SWM1" << EOF
 exit
 EOF
