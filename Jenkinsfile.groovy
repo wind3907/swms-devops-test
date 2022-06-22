1770130081cb22e6ce9a60030b917675f710b58e
Wimukthi Indeewara <wind3907>
2022-06-22 20:21:16 +0530

    test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,8 +1,8 @@
 properties(
     [
         buildDiscarder(logRotator(numToKeepStr: '20')),
-        parameters(
-            [
+        // parameters(
+        //     [
                 // string(name: 'SOURCE_DB', defaultValue: 'rsxxxe', description: 'Source Database. eg: rs040e'),
                 // string(name: 'TARGET_DB', defaultValue: 'rds_trn_xxx', description: 'Target Database. eg: rds_trn_040'),
                 // string(name: 'ROOT_PW', defaultValue: 'SwmsRoot123', description: 'Root Password'),
@@ -35,8 +35,8 @@ properties(
                 // string(name: 'dba_masterfile_names', description: 'The name of the artifact to deploy', defaultValue: 'R50_0_dba_master.sql', trim: true),
                 // string(name: 'master_file_retry_count', description: 'Amount of attempts to apply the master file. This is setup to handle circular dependencies by running the same master file multiple times.', defaultValue: '3', trim: true),
                 // separator(name: "test", sectionHeader: "Data Migration Parameters", separatorStyle: "border-color: orange;", separatorHeaderStyle: "font-weight: bold; line-height: 1.5em; font-size: 1.5em;")
-            ]
-        )
+        //     ]
+        // )
     ]
 )
 pipeline {
