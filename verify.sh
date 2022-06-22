90af23b658556fe2b91978aec6215ada677b9738
Wimukthi Indeewara <wind3907>
2022-06-22 20:39:37 +0530

    test

--- verify.sh
+++ verify.sh
@@ -8,6 +8,6 @@ echo "${ROOTPW}"
 export ORACLE_SID=swms_ci1
 export TNS_ADMIN=/home2/dba/jcx/11gtords
 
-sqlplus 'swms/swms@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST='lx739q13-db.swms-np.us-east-1.aws.sysco.net')(PORT='1521'))(CONNECT_DATA=(SID='SWM1')))' << EOF
+sqlplus 'swms/swms@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST='$TARGETDB-db.swms-np.us-east-1.aws.sysco.net')(PORT='1521'))(CONNECT_DATA=(SID='SWM1')))' << EOF
 exit
 EOF
