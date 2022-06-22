1970ad120ba28d2fef0cb1ac13592284c6746ae5
Wimukthi Indeewara <wind3907>
2022-06-22 17:16:15 +0530

    test

--- verify.sh
+++ verify.sh
@@ -1,7 +1,8 @@
 TARGETDB='lx739q13-db.swms-np.us-east-1.aws.sysco.net'
 CONNECTION="swms/swms@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$TARGETDB)(PORT='1521'))(CONNECT_DATA=(SID='SWM1')))"
 # ROOTPW="ZXCK>MW*$uo%B6t?"
-echo "$CONNECTION"
-# sqlplus """swms/swms@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$TARGETDB)(PORT='1521'))(CONNECT_DATA=(SID='SWM1')))"""  << EOF
+echo '$CONNECTION'
+echo $CONNECTION
+# sqlplus $CONNECTION  << EOF
 # exit
 # EOF
