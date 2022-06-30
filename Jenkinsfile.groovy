f0824be91f0eef517da60452c7139181b3f189aa
Wimukthi Indeewara <wind3907>
2022-06-30 21:37:13 +0530

    hi

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -32,7 +32,7 @@ pipeline {
                     sh '''
                         ssh -i $SSH_KEY ${SSH_KEY_USR}@rs1060b1.na.sysco.net "
                         . ~/.profile; 
-                        /tempfs/terraform/verify.sh '${TARGETDB}' '${ROOTPW}'
+                        /tempfs/terraform/verify.sh '${TARGET_DB}' '$ROOT_PW'
                         "
                     '''
                 }
