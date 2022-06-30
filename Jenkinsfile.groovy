c1419096228245f5a18e33d2b7d3814171627701
Wimukthi Indeewara <wind3907>
2022-06-30 18:44:13 +0530

    hi

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,6 +4,7 @@ pipeline {
         string(name: 'TARGET_DB', defaultValue: 'lx076trn', description: 'TARGET DB')
     }
     environment {
+        SSH_KEY = credentials('/swms/jenkins/swms-universal-build/svc_swmsci_000/key')
         ROOT_PW = credentials("/swms/deployment_automation/nonprod/oracle/master_creds/${params.TARGET_DB}")
     }
     stages {
@@ -17,7 +18,16 @@ pipeline {
         stage('Copy Chef Resources to S3') {
             steps {
                 script{
-                    sh(script: 'verify.sh ${TARGET_DB} $ROOT_PW'.stripIndent(),returnStatus: true)
+                    sh """
+                        ssh -i $SSH_KEY ${SSH_KEY_USR}@rs1060b1.na.sysco.net ". ~/.profile; beoracle_ci mkdir -p /tempfs/swms-devops-test"
+                        scp -i $SSH_KEY ${WORKSPACE}/verify.sh ${SSH_KEY_USR}@rs1060b1.na.sysco.net:/tempfs/swms-devops-test/
+                    """
+                    sh '''
+                        ssh -i $SSH_KEY ${SSH_KEY_USR}@rs1060b1.na.sysco.net "
+                        . ~/.profile; 
+                        /tempfs/swms-devops-test/verify.sh '${params.TARGETDB}' '$ROOTPW'
+                        "
+                    '''
                 }
             }
         }
