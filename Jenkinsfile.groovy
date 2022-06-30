d2f83fe684e60a7129064221135b04ca823604fb
Wimukthi Indeewara <wind3907>
2022-06-30 18:04:00 +0530

    hi

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,7 +17,7 @@ pipeline {
         stage('Copy Chef Resources to S3') {
             steps {
                 script{
-                    echo '$ROOT_PW'
+                    echo 'hii'
                 }
             }
         }
