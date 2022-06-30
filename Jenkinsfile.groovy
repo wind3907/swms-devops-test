9f4e7afae55483837aaa66c57faefe698f8e74df
Wimukthi Indeewara <wind3907>
2022-06-30 13:28:24 +0530

    Tes

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,8 +19,7 @@ pipeline {
             steps {
                 script{
                     chef_state = sh(script: ''' aws s3 cp s3://swms-infra-deployment/env:/lx739q17/terraform.tfstate - | jq '.resources | .[] | select(.name=="cheff_state") | .instances | .[] | .attributes.content' ''',returnStdout: true)
-                    echo "---" > "${WORKSPACE}/dev-client-rhel-7.yml"
-                    echo -e '$chef_state' >> "${WORKSPACE}/dev-client-rhel-7.yml"
+                    sh (script: '''echo -e '$chef_state' >> "${WORKSPACE}/dev-client-rhel-7.yml"''')
                     sh '''
                     aws s3api put-object --bucket swms-jenkins-chef-ci --key chef_state_files/lx739q17/dev-client-rhel-7.yml --body "${WORKSPACE}/dev-client-rhel-7.yml"
                     '''
