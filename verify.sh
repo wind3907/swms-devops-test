d32f3f148672dcabf4c76a7c30ae4a59c7358c79
Wimukthi Indeewara <wind3907>
2022-07-01 16:06:16 +0530

    hi

--- verify.sh
+++ verify.sh
@@ -1,17 +1,12 @@
 TARGETDB=$1
 ROOTPW=$2
 
-echo "$TARGETDB"
-echo "$ROOTPW"
-
-result=`sqlplus 'root/'$ROOTPW'@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST='$TARGETDB'-db.swms-np.us-east-1.aws.sysco.net')(PORT='1521'))(CONNECT_DATA=(SID='SWM1')))' << EOF
+result=`sqlplus 'root/'$ROOTPW'@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST='$TARGETDB-db.swms-np.us-east-1.aws.sysco.net')(PORT='1521'))(CONNECT_DATA=(SID='SWM1')))' << EOF
 exit
 EOF`
-
 echo $result
                                                                                               
 if echo $result | grep -q "Connected"                                                                                                    
     then echo 'Connected Successfulyy'                                                                                                            
     else exit 1                                                                                                                          
-fi  
-
+fi
