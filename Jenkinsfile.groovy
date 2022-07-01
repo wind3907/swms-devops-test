e64ce7cef1c2c07930a0239f519a8ce88243583f
Wimukthi Indeewara <wind3907>
2022-07-01 08:46:13 +0530

    hi

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,10 +6,6 @@ pipeline {
     }
     environment {
         SSH_KEY = credentials('/swms/jenkins/swms-universal-build/svc_swmsci_000/key')
-        TARGET_DB = "${params.TARGET_DB}"
-        TARGET_DB_ALIAS = "${params.TARGET_DB}_db"
-        SOURCE_DB = "rs048e"
-        AIX_DB_BK = "/tempfs/DBBackup/SWMS/swm1_db_*.tar.gz"
     }
     stages {
         stage('Checkout SCM') {
@@ -19,37 +15,23 @@ pipeline {
                 echo "Building ${env.JOB_NAME}..."
             }
         }
-        stage('Copy Chef Resources to S3') {
+        stage('Testing EC2 Connection') {
             steps {
+                echo "Testing EC2 Connection"
+                
                 script{
-                    if ("${params.ROOT_PW}" != "" ){
-                        env.ROOT_PW = "${params.ROOT_PW}"
+                    env.TARGET_DB = "lx076trn"
+                    env.HOST_IP = sh(script: '''dig +short $TARGET_DB.swms-np.us-east-1.aws.sysco.net | head -n 1''', returnStdout: true) .trim()                   
+                    if (HOST_IP != ''){
+                        echo "EC2 Instance is successfully created"
                     }else{
-                        env.ROOT_PW = sh(script: '''aws secretsmanager get-secret-value --secret-id /swms/deployment_automation/nonprod/oracle/master_creds/$TARGET_DB | jq --raw-output '.SecretString' ''',returnStdout: true).trim()
+                        echo "EC2 Instance provisioning failed"
+                        currentBuild.result = "FAILURE"
+                        sh "exit 1"       
                     }
                 }
             }
-        }
-        stage('Reset network ACLs on RDS') {
-            steps {
-                echo "Section: Reset network ACLs on RDS"
-                script {
-                    env.HOST_IP = sh(script: '''dig +short $TARGET_DB.swms-np.us-east-1.aws.sysco.net | head -n 1''', returnStdout: true) .trim()                  
-                    
-                    sh """
-                        ssh -i $SSH_KEY ${SSH_KEY_USR}@rs1060b1.na.sysco.net ". ~/.profile; beoracle_ci mkdir -p /tempfs/terraform"
-                        scp -i $SSH_KEY ${WORKSPACE}/reset_network_acls.sh ${SSH_KEY_USR}@rs1060b1.na.sysco.net:/tempfs/terraform/
-                        scp -i $SSH_KEY ${WORKSPACE}/reset_network_acls.sql ${SSH_KEY_USR}@rs1060b1.na.sysco.net:/tempfs/terraform/
-                    """
-                    sh '''
-                        ssh -i $SSH_KEY ${SSH_KEY_USR}@rs1060b1.na.sysco.net "
-                        . ~/.profile;
-                        beoracle_ci /tempfs/terraform/reset_network_acls.sh '${SOURCE_DB}' '${TARGET_DB_ALIAS}' '${ROOT_PW}' '${HOST_IP}' '${AIX_DB_BK}'
-                        "
-                    '''
-                }
-            }
-        }
+        } 
         // stage('Print') {
         //     steps {
         //         script{
