b9d2186e5c25d9d7ede9353e08f3981d8df4d535
Wimukthi Indeewara <wind3907>
2022-07-01 14:28:15 +0530

    hi

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,5 @@
 pipeline {
-    agent { label 'master' }
+    agent { label 'terraform-slave' }
     parameters {
         string(name: 'TARGET_DB', defaultValue: 'lx076trn', description: 'TARGET DB')
         string(name: 'ROOT_PW', defaultValue: '', description: 'TARGET DB')
@@ -16,19 +16,28 @@ pipeline {
             }
         }
         stage('Testing EC2 Connection') {
+            when {
+                expression {
+                    params.TERRAFORM_COMMAND == 'create'
+                }
+            }
             steps {
                 echo "Testing EC2 Connection"
                 
                 script{
-                    env.TARGET_DB = "lx07trn"
-                    env.HOST_IP = sh(script: '''dig +short $TARGET_DB.swms-np.us-east-1.aws.sysco.net | head -n 1''', returnStdout: true) .trim()                   
-                    if (HOST_IP != ''){
-                        echo "EC2 Instance is successfully created"
-                    }else{
-                        echo "EC2 Instance provisioning failed"
-                        currentBuild.result = "FAILURE"
-                        sh "exit 1"       
-                    }
+                    env.TARGETDB = "lx739q18"
+                    env.ROOTPW = sh(script: '''aws secretsmanager get-secret-value --secret-id /swms/deployment_automation/nonprod/oracle/master_creds/lx739q18 --region us-east-1 | jq --raw-output '.SecretString' ''',returnStdout: true).trim()
+                    
+                    sh """
+                        ssh -i $SSH_KEY ${SSH_KEY_USR}@rs1060b1.na.sysco.net ". ~/.profile; beoracle_ci mkdir -p /tempfs/terraform"
+                        scp -i $SSH_KEY ${WORKSPACE}/verify.sh ${SSH_KEY_USR}@rs1060b1.na.sysco.net:/tempfs/terraform/
+                    """
+                    sh '''
+                        ssh -i $SSH_KEY ${SSH_KEY_USR}@rs1060b1.na.sysco.net "
+                        . ~/.profile; 
+                        /tempfs/terraform/verify.sh '${TARGETDB}' '${ROOTPW}'
+                        "
+                    '''
                 }
             }
         } 
