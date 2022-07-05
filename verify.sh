987ea0aef9bff5810008b46b25c1a12bf10a1594
Wimukthi Indeewara <wind3907>
2022-07-05 08:08:44 +0530

    test

--- verify.sh
+++ verify.sh
@@ -2,7 +2,7 @@ TARGETDB=$1
 ROOTPW=$2
 
 records=`grep -c lx059trn_db /home2/dba/jcx/11gtords/tnsnames.ora`
-if [ $records == 0 ]                                                                                                    
+if [ $records == '0' ]                                                                                                    
 then
     echo "Record is already in tnsnames"                                                                                                           
 else
