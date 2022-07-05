6fd80b3a1554a48976f4995dc6478b53051b471c
Wimukthi Indeewara <wind3907>
2022-07-05 08:10:12 +0530

    test

--- verify.sh
+++ verify.sh
@@ -1,7 +1,7 @@
 TARGETDB=$1
 ROOTPW=$2
 
-records=`grep -c lx059trn_db /home2/dba/jcx/11gtords/tnsnames.ora`
+records=`grep -c lx0349trn_db /home2/dba/jcx/11gtords/tnsnames.ora`
 if [ $records == '0' ]                                                                                                    
 then
     echo "Record is not in tnsnames"                                                                                        
