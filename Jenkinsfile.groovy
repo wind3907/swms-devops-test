00d40a2e5d9434bb5e6f113983a2bf0bdbe49004
Wimukthi Indeewara <wind3907>
2022-07-05 18:50:04 +0530

    This is complete

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -15,7 +15,9 @@ pipeline {
             steps {
                 script{
                     env.TEST="Example varibale"
-                    env.EMAIL='wimukthibw@gmail.com'
+                    env.OPCO="lx076trn"
+                    env.EMAIL = sh(script: "grep $OPCO  ${WORKSPACE}/email_recipients.txt | awk '{print $2}'",returnStdout: true).trim()
+                    echo $EMAIL
                 }
             }
         } 
@@ -24,10 +26,10 @@ pipeline {
         success {
             script {
                 echo 'Data migration from Oracle 11 AIX to Oracle 19 RDS is successful!'
-                emailext body: 'Project: $PROJECT_NAME <br/>Build # $BUILD_NUMBER <br/>Status: $BUILD_STATUS <br/>TEST ENV: ${ENV,var="TEST"} <br/>Check console output at $BUILD_URL to view the results.',
-                    mimeType: 'text/html',
-                    subject: "[SWMS-OPCO-DEPLOYMENT] - ${currentBuild.fullDisplayName}",
-                    to: '${ENV,var="EMAIL"}'
+                // emailext body: 'Project: $PROJECT_NAME <br/>Build # $BUILD_NUMBER <br/>Status: $BUILD_STATUS <br/>TEST ENV: ${ENV,var="TEST"} <br/>Check console output at $BUILD_URL to view the results.',
+                //     mimeType: 'text/html',
+                //     subject: "[SWMS-OPCO-DEPLOYMENT] - ${currentBuild.fullDisplayName}",
+                //     to: '${ENV,var="EMAIL"}'
             }
         }
         failure {
