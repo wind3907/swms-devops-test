653fba64285e0e6e32f58ffa6a33d44967cbeb92
Wimukthi Indeewara <wind3907>
2022-07-05 08:26:08 +0530

    test

--- verify.sh
+++ verify.sh
@@ -1,10 +1,22 @@
-TARGETDB=$1
+TARGETDB='lx739q17_db'
 ROOTPW=$2
 
-records=`grep -c lx0349trn_db /home2/dba/jcx/11gtords/tnsnames.ora`
+records=`grep -c $TARGETDB /home2/dba/jcx/11gtords/tnsnames-test.ora`
 if [ $records == '0' ]                                                                                                    
 then
-    echo "Record is not in tnsnames"                                                                                        
+    echo "Record is not in tnsnames"
+
 else
-    echo "Record is already in tnsnames"       
+    echo "Record is already in tnsnames"
+    cat <> /home2/dba/jcx/11gtords/tnsnames-test.ora
+    $TARGETDB =
+    (DESCRIPTION =
+        (ADDRESS_LIST =
+        (ADDRESS = (PROTOCOL = TCP)(HOST =$TARGETDB-db.coz2zoxeiq71.us-east-1.rds.amazonaws.com)(PORT = 1521))
+        )
+    (CONNECT_DATA =
+        (SID = swm1)
+        )
+    )
+    EOF       
 fi
