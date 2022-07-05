a61bb2c89ed15f2cb028d96423ec2670a66257bc
Wimukthi Indeewara <wind3907>
2022-07-05 18:23:52 +0530

    This is complete

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,9 +1,5 @@
 pipeline {
     agent { label 'master' }
-    parameters {
-        string(name: 'TARGET_DB', defaultValue: 'lx076trn', description: 'TARGET DB')
-        string(name: 'ROOT_PW', defaultValue: '', description: 'TARGET DB')
-    }
     environment {
         SSH_KEY = credentials('/swms/jenkins/swms-universal-build/svc_swmsci_000/key')
     }
@@ -15,40 +11,21 @@ pipeline {
                 echo "Building ${env.JOB_NAME}..."
             }
         }
-        stage('Prepare db export to RDS') {
+        stage('email') {
             steps {
-                echo "Section: Prepare db export to RDS"
-                sh "scp -i $SSH_KEY ${WORKSPACE}/tnsnames.sh ${SSH_KEY_USR}@rs1060b1.na.sysco.net:/tempfs"
-                sh """
-                    ssh -i $SSH_KEY ${SSH_KEY_USR}@rs1060b1.na.sysco.net "
-                    . ~/.profile;
-                    beoracle_ci /tempfs/tnsnames.sh
-                    "
-                """
+                env.TEST="Example varibale"
+                echo "Section: amil"
             }
         } 
-        // stage('Print') {
-        //     steps {
-        //         script{
-        //             echo "$ROOT_PW"
-        //             sh """
-        //                 ssh -i $SSH_KEY ${SSH_KEY_USR}@rs1060b1.na.sysco.net ". ~/.profile; beoracle_ci mkdir -p /tempfs/terraform"
-        //                 scp -i $SSH_KEY ${WORKSPACE}/verify.sh ${SSH_KEY_USR}@rs1060b1.na.sysco.net:/tempfs/terraform/
-        //             """
-        //             sh '''
-        //                 ssh -i $SSH_KEY ${SSH_KEY_USR}@rs1060b1.na.sysco.net "
-        //                 . ~/.profile; 
-        //                 /tempfs/terraform/verify.sh '${TARGET_DB}' "$ROOT_PW"
-        //                 "
-        //             '''
-        //         }
-        //     }
-        // }
     }
     post {
         success {
             script {
                 echo 'Data migration from Oracle 11 AIX to Oracle 19 RDS is successful!'
+                emailext body: 'Project: $PROJECT_NAME <br/>Build # $BUILD_NUMBER <br/>Status: $BUILD_STATUS <br/>TEST ENV: ${ENV,var="TEST"} <br/>Check console output at $BUILD_URL to view the results.',
+                    mimeType: 'text/html',
+                    subject: "[SWMS-OPCO-DEPLOYMENT] - ${currentBuild.fullDisplayName}",
+                    to: 'wimukthibw@gmail.com'
             }
         }
         failure {
