0497b6e6d08d0c53c8e5f9729fcf16ca02708f7c
Wimukthi Indeewara <wind3907>
2022-07-06 11:54:17 +0530

    schedule

--- data_migration_schedule.groovy
+++ data_migration_schedule.groovy
@@ -1,5 +1,10 @@
-def schedule() {
-    return "Hello schedule"
+def schedule = '''
+            # lx036trn - Every monday 11.30 pm UTF time
+            30 23 * * 1 %SOURCE_DB=rs048e;TARGET_DB=lx036trn;TARGET_SERVER=lx036trn
+        '''
+
+def getSchedule() {
+    return schedule
 }
 def printHello() {
   println "Hello, world!"
