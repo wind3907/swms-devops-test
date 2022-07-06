3184a3456554d17ef59c6c079bcdf11882ab186d
Wimukthi Indeewara <wind3907>
2022-07-06 17:14:51 +0530

    hi

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,14 +19,14 @@ pipeline {
                     url: "https://github.com/wind3907/cron.git"
                 }
                 sh "cat email-repo/email_recipients.txt"
-                // env.TEST="Example varibale"
-                // env.OPCO="lx036trn"
-                // env.EMAIL = sh(script: '''grep lx036trn $WORKSPACE/email_recipients.txt | awk '{ print $2 }' ''',returnStdout: true).trim()  
-                // echo 'Data migration from Oracle 11 AIX to Oracle 19 RDS is successful!'
-                // emailext body: 'Project: $PROJECT_NAME <br/>Build # $BUILD_NUMBER <br/>Status: $BUILD_STATUS <br/>TEST ENV: ${ENV,var="TEST"} <br/>Check console output at $BUILD_URL to view the results.',
-                //     mimeType: 'text/html',
-                //     subject: "[SWMS-DATA-MIGRATION-AIX-RDS] - ${currentBuild.fullDisplayName}",
-                //     to: '${ENV,var="EMAIL"}'
+                env.TEST="Example varibale"
+                env.OPCO="lx036trn"
+                env.EMAIL = sh(script: '''grep lx036trn email-repo/email_recipients.txt | awk '{ print $2 }' ''',returnStdout: true).trim()  
+                echo 'Data migration from Oracle 11 AIX to Oracle 19 RDS is successful!'
+                emailext body: 'Project: $PROJECT_NAME <br/>Build # $BUILD_NUMBER <br/>Status: $BUILD_STATUS <br/>TEST ENV: ${ENV,var="TEST"} <br/>Check console output at $BUILD_URL to view the results.',
+                    mimeType: 'text/html',
+                    subject: "[SWMS-DATA-MIGRATION-AIX-RDS] - ${currentBuild.fullDisplayName}",
+                    to: '${ENV,var="EMAIL"}'
             }
         }
         failure {
