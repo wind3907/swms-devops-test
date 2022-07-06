23cee5a2b27096bac99c6f48dcb938df3f6dabe7
Wimukthi Indeewara <wind3907>
2022-07-06 17:55:27 +0530

    This is a test commit

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,7 @@ properties(
         buildDiscarder(logRotator(numToKeepStr: '20')),
         parameters(
             [
-                string(name: 'branch', description: 'The rf-host-service branch to be built and included', defaultValue: 'develop',  trim: true),
+                string(name: 'TARGET_DB', description: 'The rf-host-service branch to be built and included', defaultValue: 'lx036trn',  trim: true),
             ]
         )
     ]
@@ -28,24 +28,22 @@ pipeline {
                     credentialsId: scm.getUserRemoteConfigs()[0].getCredentialsId(),
                     url: "https://github.com/wind3907/cron.git"
                 }
-                env.TEST="Example varibale"
-                sh "echo ${params.branch}"
-                // env.OPCO="lx036trn"
-                // env.EMAIL = sh(script: '''grep lx036trn email-repo/email_recipients.txt | awk '{ print $2 }' ''',returnStdout: true).trim()  
-                // echo 'Data migration from Oracle 11 AIX to Oracle 19 RDS is successful!'
-                // emailext body: 'Project: $PROJECT_NAME <br/>Build # $BUILD_NUMBER <br/>Status: $BUILD_STATUS <br/>TEST ENV: ${ENV,var="TEST"} <br/>Check console output at $BUILD_URL to view the results.',
-                //     mimeType: 'text/html',
-                //     subject: "[SWMS-DATA-MIGRATION-AIX-RDS] - ${currentBuild.fullDisplayName}",
-                //     to: '${ENV,var="EMAIL"}'
 
-                // office365ConnectorSend webhookUrl: 'https://sysco.webhook.office.com/webhookb2/ea773582-604e-4b79-bae7-681359dc45b6@b7aa4308-bf33-414f-9971-6e0c972cbe5d/JenkinsCI/3d4a4f7ca2c142e48f993d7b37e7028a/52b05ab5-3f6f-48ee-a10f-1957d1592c08',
-                //         message: "Build # ${currentBuild.id}",
-                //         factDefinitions: [[name: "Remarks", template: "${currentBuild.getBuildCauses()[0].shortDescription}"],
-                //                          [name: "Last Commit", template: "${sh(returnStdout: true, script: 'git -C swms-devops-test log -1 --pretty=format:%h')}"],
-                //                          [name: "Last Commit Author", template: "${sh(returnStdout: true, script: 'git -C swms-devops-test log -1 --pretty=format:%an')}"]
-                //                          [name: "Target Database", template: "${(env.DEPLOY_TARGET_SERVER == '-') ? '' : env.DEPLOY_TARGET_SERVER} (${env.DEPLOY_STATUS ?: 'SKIPPED'})"]],
-                //         color: (currentBuild.currentResult == 'SUCCESS') ? '#11fa1d' : '#FA113D',
-                //         status: currentBuild.currentResult
+                env.EMAIL = sh(script: '''grep ${params.TARGET_DB} email-repo/email_recipients.txt | awk '{ print $2 }' ''',returnStdout: true).trim()  
+                echo 'Data migration from Oracle 11 AIX to Oracle 19 RDS is successful!'
+                emailext body: 'Project: $PROJECT_NAME <br/>Build # $BUILD_NUMBER <br/>Status: $BUILD_STATUS <br/>Target Database ENV: ${params.TARGET_DB} <br/>Check console output at $BUILD_URL to view the results.',
+                    mimeType: 'text/html',
+                    subject: "[SWMS-DATA-MIGRATION-AIX-RDS] - ${currentBuild.fullDisplayName}",
+                    to: '${ENV,var="EMAIL"}'
+
+                office365ConnectorSend webhookUrl: 'https://sysco.webhook.office.com/webhookb2/ea773582-604e-4b79-bae7-681359dc45b6@b7aa4308-bf33-414f-9971-6e0c972cbe5d/JenkinsCI/3d4a4f7ca2c142e48f993d7b37e7028a/52b05ab5-3f6f-48ee-a10f-1957d1592c08',
+                        message: "Build # ${currentBuild.id}",
+                        factDefinitions: [[name: "Remarks", template: "${currentBuild.getBuildCauses()[0].shortDescription}"],
+                                         [name: "Last Commit", template: "${sh(returnStdout: true, script: 'git -C swms-devops-test log -1 --pretty=format:%h')}"],
+                                         [name: "Last Commit Author", template: "${sh(returnStdout: true, script: 'git -C swms-devops-test log -1 --pretty=format:%an')}"]
+                                         [name: "Target Database", template: "${params.TARGET_DB}"]],
+                        color: (currentBuild.currentResult == 'SUCCESS') ? '#11fa1d' : '#FA113D',
+                        status: currentBuild.currentResult
             }
         }
         failure {
