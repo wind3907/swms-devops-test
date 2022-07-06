5c5b415dc4bbe69c254fb475fda121129496be3f
Wimukthi Indeewara <wind3907>
2022-07-06 18:18:25 +0530

    This is a test commit 2

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -31,11 +31,11 @@ pipeline {
                 env.TARGET_DB = "${params.TARGET_DB}"
                 env.EMAIL = sh(script: '''grep $TARGET_DB email-repo/email_recipients.txt | awk '{ print $2 }' ''',returnStdout: true).trim()  
                 echo 'Data migration from Oracle 11 AIX to Oracle 19 RDS is successful!'
-                sh "echo $EMAIL"
+                
                 emailext body: 'Project: $PROJECT_NAME <br/>Build # $BUILD_NUMBER <br/>Status: $currentBuild.currentResult <br/>Target Database ENV: ${params.TARGET_DB} <br/>Check console output at $BUILD_URL to view the results.',
-                    mimeType: 'text/html',
-                    subject: "[SWMS-DATA-MIGRATION-AIX-RDS] - ${currentBuild.fullDisplayName}",
-                    to: '${ENV,var="EMAIL"}'
+                mimeType: 'text/html',
+                subject: "[SWMS-DATA-MIGRATION-AIX-RDS] - ${currentBuild.fullDisplayName}",
+                to: '${ENV,var="EMAIL"}'
 
                 office365ConnectorSend webhookUrl: 'https://sysco.webhook.office.com/webhookb2/ea773582-604e-4b79-bae7-681359dc45b6@b7aa4308-bf33-414f-9971-6e0c972cbe5d/JenkinsCI/3d4a4f7ca2c142e48f993d7b37e7028a/52b05ab5-3f6f-48ee-a10f-1957d1592c08',
                         message: "Build # ${currentBuild.id}",
