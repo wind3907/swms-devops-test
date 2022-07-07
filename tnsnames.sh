0da6f36b9bae45d1a3d445e4d92c87eb32e9d454
Wimukthi Indeewara <wind3907>
2022-07-07 10:48:35 +0530

    Test email and teams notification

--- tnsnames.sh
+++ tnsnames.sh
@@ -5,7 +5,17 @@ echo "${records}"
 if [ $records == '0' ]                                                                                                    
 then
     echo "Record is not in tnsnames"
-    echo "${TARGETDB}_db = (DESCRIPTION =(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST =$TARGETDB-db.coz2zoxeiq71.us-east-1.rds.amazonaws.com)(PORT = 1521)))(CONNECT_DATA =(SID = swm1)))" >> /tempfs/tnsnames-test.ora
+    cat <<EOT >> /tempfs/tnsnames-test.ora
+    lx739q17_db =
+        (DESCRIPTION =
+            (ADDRESS_LIST =
+            (ADDRESS = (PROTOCOL = TCP)(HOST =lx739q17-db.coz2zoxeiq71.us-east-1.rds.amazonaws.com)(PORT = 1521))
+            )
+        (CONNECT_DATA =
+            (SID = swm1)
+            )
+        )
+    EOT
 else
     echo "Record is already in tnsnames"
 fi
