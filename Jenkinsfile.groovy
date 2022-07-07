024ad1f65aae589747a9c5dff920b8bf6ded2d09
Wimukthi Indeewara <wind3907>
2022-07-07 10:25:29 +0530

    Test email and teams notification

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -14,50 +14,29 @@ pipeline {
     options {
         skipDefaultCheckout()
     }
+    environment {
+        SSH_KEY = credentials('/swms/jenkins/swms-universal-build/svc_swmsci_000/key')
+    }
     stages {
         stage('Checkout SCM') {
             steps {
                 dir('swms-devops-test') { checkout scm }
             }
         }
-        stage('Test'){
+        stage('Tnsnames Configuration') {
             steps {
-                script {
-                    sh "echo 'This is a test'"
-                    sh "exit 1"
-                }
-            }
-        } 
-    }
-    post {
-        always {
-            script {
-                env.SOURCE_DB = 'rs048e'
-                env.TARGET_DB = 'lx048trn'
-                dir("selector-academy") {
-                    git branch: "master",
-                    credentialsId: '4c5daf94-f77a-4854-8a88-03fae213f59b',
-                    url: "https://github.com/SyscoCorporation/selector-academy.git"
-                }
-                env.EMAIL = sh(script: '''grep $TARGET_DB selector-academy/email_recipients.txt | awk '{ print $2 }' ''',returnStdout: true).trim()
-                emailext body: 'Project: $PROJECT_NAME <br/>Build # $BUILD_NUMBER <br/>Status: $BUILD_STATUS <br/>Source Database: $SOURCE_DB <br/>Target Database: $TARGET_DB <br/>Check console output at $BUILD_URL to view the results.',
-                    mimeType: 'text/html',
-                    subject: "[SWMS-DATA-MIGRATION-AIX-RDS] - ${currentBuild.fullDisplayName}",
-                    to: '${ENV,var="EMAIL"}'
-                
-                withCredentials([string(credentialsId: '/swms/jenkins/swms-data-migration-aix-rds', variable: 'TEAMS_WEBHOOK_URL')]) {
-                    office365ConnectorSend webhookUrl: TEAMS_WEBHOOK_URL,
-                        message: "Build # ${currentBuild.id}",
-                        factDefinitions: [[name: "Remarks", template: "${currentBuild.getBuildCauses()[0].shortDescription}"],
-                                         [name: "Last Commit", template: "${sh(returnStdout: true, script: 'git -C swms-devops-test log -1 --pretty=format:%h')}"],
-                                         [name: "Last Commit Author", template: "${sh(returnStdout: true, script: 'git -C swms-devops-test log -1 --pretty=format:%an')}"],
-                                         [name: "Source Database", template: "${params.SOURCE_DB}"],
-                                         [name: "Target Database", template: "${params.TARGET_DB}"]],
-                        color: (currentBuild.currentResult == 'SUCCESS') ? '#11fa1d' : '#FA113D',
-                        status: currentBuild.currentResult
-                }
+                echo "Section: Tnsnames Configuration"
+                sh "scp -i $SSH_KEY ${WORKSPACE}/tnsnames.sh ${SSH_KEY_USR}@rs1060b1.na.sysco.net:/tempfs"
+                sh '''
+                    ssh -i $SSH_KEY ${SSH_KEY_USR}@rs1060b1.na.sysco.net "
+                    . ~/.profile;
+                    beoracle_ci /tempfs/tnsnames.sh
+                    "
+                '''
             }
         }
+    }
+    post {
         success {
             script {
                 echo 'Data migration from Oracle 11 AIX to Oracle 19 RDS is Success'
--- tnsnames.sh
+++ tnsnames.sh
@@ -1,11 +1,10 @@
 TARGETDB='lx739q17'
-ROOTPW=$2
 
-records=`grep -c "${TARGETDB}_db" /home2/dba/jcx/11gtords/tnsnames.ora`
+records=`grep -c "${TARGETDB}_db" /tempfs/tnsnames-test.ora`
 if [ $records == '0' ]                                                                                                    
 then
     echo "Record is not in tnsnames"
-    echo "${TARGETDB}_db = (DESCRIPTION =(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST =$TARGETDB-db.coz2zoxeiq71.us-east-1.rds.amazonaws.com)(PORT = 1521)))(CONNECT_DATA =(SID = swm1)))" >> /home2/dba/jcx/11gtords/tnsnames.ora
+    echo "${TARGETDB}_db = (DESCRIPTION =(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST =$TARGETDB-db.coz2zoxeiq71.us-east-1.rds.amazonaws.com)(PORT = 1521)))(CONNECT_DATA =(SID = swm1)))" >> /tempfs/tnsnames-test.ora
 else
     echo "Record is already in tnsnames"
 fi
