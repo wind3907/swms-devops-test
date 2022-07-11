db3e517406f7f9be0fae759f7d93e377722d3eed
Wimukthi Indeewara <wind3907>
2022-07-11 09:19:04 +0530

    Ho

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -25,7 +25,7 @@ pipeline {
     post {
         always {
             script {
-                sh(script: 'echo $BUILD_STATUS',returnStdout: true)
+                sh(script: "echo $BUILD_STATUS",returnStdout: true)
                 def props = readProperties  file: "${WORKSPACE}/email.properties"
                 def SUBJECT = props['subject_successfull']
                 def BODY = props['body']
