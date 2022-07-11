5cc8b9b5952a5c4e84287d63ac2098450f28e1ed
Wimukthi Indeewara <wind3907>
2022-07-11 12:15:03 +0530

    Ho

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,3 +1,14 @@
+properties(
+    [
+        buildDiscarder(logRotator(numToKeepStr: '20')),
+        parameters(
+            [
+                string(name: 'TARGET_DB', description: 'The rf-host-service branch to be built and included', defaultValue: 'lx048trn',  trim: true),
+                string(name: 'SOURCE_DB', description: 'The rf-host-service branch to be built and included', defaultValue: 'rs048e',  trim: true),
+            ]
+        )
+    ]
+)
 pipeline {
     agent { label 'master' }
     environment {
@@ -12,7 +23,8 @@ pipeline {
                     env.INSTANCE_DB = "lx222trn-db"
                     def INSTANCE_ID = sh(script: "aws ec2 describe-instances --filters 'Name=tag:Name,Values=$INSTANCE' --query Reservations[*].Instances[*].[InstanceId] --output text --region us-east-1", returnStdout: true).trim()
                     def INSTANCE_DB_ARN = sh(script: "aws rds describe-db-instances --db-instance-identifier $INSTANCE_DB --query DBInstances[*].[DBInstanceArn] --output text --region us-east-1", returnStdout: true).trim()
-                    echo "$INSTANCE_DB_ARN"
+                    sh "aws ec2 create-tags --resources ${INSTANCE_ID} --tags Key='Automation:PMC',Value='Always On' --region us-east-1"
+                    sh "aws rds add-tags-to-resource --resource-name ${INSTANCE_DB_ARN} --tags Key='Automation:PMC',Value='Always On' --region us-east-1"
                 }
             }
         }
