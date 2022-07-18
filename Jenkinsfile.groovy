a85d879255f42a35d2cab9a8a6c17a595b4decd3
Wimukthi Indeewara <wind3907>
2022-07-18 17:15:24 +0530

    Added service user env for tns configuration

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -46,10 +46,9 @@ pipeline {
                 script {
                     echo "$SWMS_VERSION"
                     env.SWMS_VERSION_NUMBER = "${env.SWMS_VERSION.substring(0,env.SWMS_VERSION.length()-2).replaceAll('\\.', '_')}"
-                    // env.process = sh (script: "aws s3api list-objects --bucket swms-build-artifacts --prefix linux-${SWMS_VERSION_NUMBER} --query Contents[].Key", returnStdout: true ).trim()
                     def process = "aws s3api list-objects --bucket swms-build-artifacts --prefix linux-${SWMS_VERSION_NUMBER} --query Contents[].Key".execute()
-                    def version = process.text.replaceAll('"', "'").replaceAll("\\n","").replaceAll(" ","").tokenize(',[]')
-                    echo "${version[0]}"
+                    env.version = process.text.replaceAll('"', "'").replaceAll("\\n","").replaceAll(" ","").tokenize(',[]')[0]
+                    echo "$version"
                 }
             }
         }
