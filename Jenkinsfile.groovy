1bf77483de7fb37d39c945820d9d873be9b82a2a
Wimukthi Indeewara <wind3907>
2022-07-19 06:43:10 +0530

    test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,6 +5,7 @@ properties(
             [
                 string(name: 'TARGET_DB', description: 'The rf-host-service branch to be built and included', defaultValue: 'lx076trn',  trim: true),
                 string(name: 'SOURCE_DB', description: 'The rf-host-service branch to be built and included', defaultValue: 'rs048e',  trim: true),
+                string(name: 'ROOT_PW', defaultValue: '', description: 'Root Password'),
             ]
         )
     ]
@@ -18,24 +19,20 @@ pipeline {
     stages {
         stage("Get Production Version") {
             environment {
-                ORACLE_SWMS_USR_SECRET_PATH = "/swms/deployment_automation/nonprod/oracle/user/swms"
+                ORACLE_SWMS_USR_SECRET_PATH = "/swms/deployment_automation/nonprod/oracle/master_creds/${TARGET_DB}"
             }
             steps {
                 echo "Section: Get Production Version"
                 script {
-                    withCredentials([usernamePassword(credentialsId: "${ORACLE_SWMS_USR_SECRET_PATH}", usernameVariable: 'ORACLE_SWMS_USER', passwordVariable: 'ORACLE_SWMS_PASSWORD')]) {
-                        script {
-                            def status = sh(script: '''
-                                scp -i $SSH_KEY ${WORKSPACE}/rds_configurations.sh ${SSH_KEY_USR}@${TARGET_DB}.swms-np.us-east-1.aws.sysco.net:/tempfs
-                                ssh -i $SSH_KEY ${SSH_KEY_USR}@${TARGET_DB}.swms-np.us-east-1.aws.sysco.net "
-                                . /etc/profile;
-                                export ORACLE_SWMS_USER="$ORACLE_SWMS_USER";
-                                export ORACLE_SWMS_PASSWORD="$ORACLE_SWMS_PASSWORD";
-                                beoracle_ci /tempfs/rds_configurations.sh
-                                "
-                            ''', returnStdout: true).trim() 
-                            env.SWMS_VERSION = sh(script: "echo ${status} | grep 'Version Number' | cut -d' ' -f3 ", returnStdout: true).trim()
+                    if ("${params.ROOT_PW}" != "" ){
+                        env.ORACLE_DBA_PASSWORD = "${params.ROOT_PW}"
+                    }else{
+                        withCredentials([usernamePassword(credentialsId: "${ORACLE_DBA_USR_SECRET_PATH}", usernameVariable: 'ORACLE_DBA_USER', passwordVariable: 'ORACLE_DBA_PASSWORD')]) {
+                            script {
+                                env.ORACLE_DBA_PASSWORD = ORACLE_DBA_PASSWORD
+                            }
                         }
+                        
                     }
                 }
             }
@@ -44,11 +41,7 @@ pipeline {
             steps {
                 echo "Section: Test Env"
                 script {
-                    echo "$SWMS_VERSION"
-                    env.SWMS_VERSION_NUMBER = "${env.SWMS_VERSION.substring(0,env.SWMS_VERSION.length()-2).replaceAll('\\.', '_')}"
-                    def process = "aws s3api list-objects --bucket swms-build-artifacts --prefix linux-${SWMS_VERSION_NUMBER} --query Contents[].Key".execute()
-                    env.version = process.text.replaceAll('"', "'").replaceAll("\\n","").replaceAll(" ","").tokenize(',[]')[0]
-                    echo "$version"
+                    echo "${ORACLE_DBA_PASSWORD}"
                 }
             }
         }
